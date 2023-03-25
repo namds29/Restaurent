59fad2cefeff80ceb4415209936f745d3fbfbfff
unknown <sinamd29@gmail.com>
2023-03-25 20:16:58 +0700

    a

--- BE/connect.js
+++ BE/connect.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Harveynash@123',
+    password: 'sinam123',
     database: "shopify",
 }).promise();
 
